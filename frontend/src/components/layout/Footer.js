1e0fb7df5570d280b3bf119d30dde9dfe7eeb325
Vansh Varshney <vsq.asap@gmail.com>
2022-05-12 15:57:22 +0530

    Build Update

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <Fragment>
       <footer className="py-1">
         <p className="text-center mt-1">
-          © V-Cart by Vansh Varshney 2021, All Rights Reserved
+          V-Cart, Made With ❤ , ©2022 All Rights Reserved
         </p>
       </footer>
     </Fragment>
